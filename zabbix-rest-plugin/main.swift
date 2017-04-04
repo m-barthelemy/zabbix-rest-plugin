e7735dffcd39f8849bb68eb60b2aa1fed9488e5e
Matthieu Barthélemy <bonsouere@gmail.com>
2017-04-04 17:52:06 -0500

    Initial Commit

---
+++ zabbix-rest-plugin/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  zabbix-rest-plugin
+//
+//  Created by Matthieu Barthélemy on 4/4/17.
+//  Copyright © 2017 mba. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
